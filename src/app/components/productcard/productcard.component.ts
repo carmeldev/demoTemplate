50d2c5bc6aa8b3860010e8a1b389ae5691ce1ceb
Priya S.P <pitchammal.priya@centizen.com>
2020-03-02 14:35:44 +0530

    product card changes

--- src/app/components/productcard/productcard.component.ts
+++ src/app/components/productcard/productcard.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductcardComponent implements OnInit {
 
+  img1 = 'http://images.the-house.com/nike-lunar-stefan-janoski-shoes-game-royal-white-15-1.jpg';
+  name1 = 'Nike Lunar Stefan Janoski Skate Shoes';
+  type1 = 'Type: Regular Height, Casual Sneakers';
+  price1 = 'Price: $ 55';
+  size1 = 'Size: Medium';
+  offer1 = 'Offer: 50%';
+  button1 = 'Buy';
+  button2 = 'Add to Cart';
   constructor() { }
 
   ngOnInit(): void {

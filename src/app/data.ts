860d463dd42ff92cc5f395c52d0efb2b911a636f
Priya S.P <pitchammal.priya@centizen.com>
2020-03-04 15:04:46 +0530

    images added into assets

--- src/app/data.ts
+++ src/app/data.ts
@@ -1,13 +1,13 @@
 export const productsList = [
-  { name: "Nike", type: "women", price: 50, rating: 4.5, imageUrl: "", size: 7, offer: 50, category: "shoe" },
-  { name: "Nike", type: "women", price: 50, rating: 4.5, imageUrl: "", size: 7, offer: 50, category: "shoe" },
-  { name: "Nike", type: "women", price: 50, rating: 4.5, imageUrl: "", size: 7, offer: 50, category: "shoe" },
-  { name: "Nike", type: "women", price: 50, rating: 4.5, imageUrl: "", size: 7, offer: 50, category: "shoe" },
+  { name: 'Nike', type: 'women', price: 50, rating: 4.5, imageUrl: '', size: 7, offer: 50, category: 'shoe' },
+  { name: 'Nike', type: 'women', price: 50, rating: 4.5, imageUrl: '', size: 7, offer: 50, category: 'shoe' },
+  { name: 'Nike', type: 'women', price: 50, rating: 4.5, imageUrl: '', size: 7, offer: 50, category: 'shoe' },
+  { name: 'Nike', type: 'women', price: 50, rating: 4.5, imageUrl: '', size: 7, offer: 50, category: 'shoe' },
 ];
 export const productDetails = [{
-  img: 'http://images.the-house.com/nike-lunar-stefan-janoski-shoes-game-royal-white-15-1.jpg',
+  img: '../../../assets/nike shoe.PNG',
   name: 'Nike Lunar Stefan Janoski Skate Shoes',
-  type: 'Regular Height, Casual Sneakers',
+  type: 'Casual Sneakers',
   price: '$ 55',
   size: 'Medium',
   offer: '50%',
@@ -16,7 +16,7 @@ export const productDetails = [{
   description: 'Special Technologies: Mens Nike SB Zoom Stefan Janoski Canvas Skateboarding Shoe LOW-PROFILE COMFORT. Mens Nike SB Zoom Stefan Janoski Canvas Skateboarding Shoe has a low-profile silhouette that delivers breathability, grip and responsive cushioning without sacrificing boardfeel. Streamlined canvas upper provides breathability. Zoom Air unit in the sockliner delivers responsive cushioning. Rubber outsole with herringbone tread pattern offers superior grip.'
 },
 {
-  img: 'http://4.bp.blogspot.com/-6VHcwOb2jnE/UMm84nhvaVI/AAAAAAAAEqo/nWNpQ8HASXM/s1600/nike_ladies_pink_shoes.JPG',
+  img: '../../../assets/nike_ladies_pink_shoes.JPG',
   name: 'New Brands Nike Shoes 4 Winter Season',
   type: 'Winter shoe',
   price: '$ 60',
@@ -24,10 +24,10 @@ export const productDetails = [{
   offer: '30%',
   category: 'Women',
   // tslint:disable-next-line: max-line-length
-  description: 'Special Technologies: Mens Nike SB Zoom Stefan Janoski Canvas Skateboarding Shoe LOW-PROFILE COMFORT. Mens Nike SB Zoom Stefan Janoski Canvas Skateboarding Shoe has a low-profile silhouette that delivers breathability, grip and responsive cushioning without sacrificing boardfeel. Streamlined canvas upper provides breathability. Zoom Air unit in the sockliner delivers responsive cushioning. Rubber outsole with herringbone tread pattern offers superior grip.'
+  description: 'Be it a party, a formal event or a casual occasion that you are dressing up for, it is very important to team your outfit with the right pair of winter shoes for women in order to be dressed for the occasion'
 },
 {
-  img: 'http://images.esellerpro.com/2379/I/697/71/KS40005S%20SILVER.jpg',
+  img: '../../../assets/silver sandal.jpg',
   name: 'High Mid Heel Diamante Party Sandals',
   type: 'White sandals',
   price: '$ 48',
@@ -35,16 +35,28 @@ export const productDetails = [{
   offer: '20%',
   category: 'Kids',
   // tslint:disable-next-line: max-line-length
-  description: 'Special Technologies: Mens Nike SB Zoom Stefan Janoski Canvas Skateboarding Shoe LOW-PROFILE COMFORT. Mens Nike SB Zoom Stefan Janoski Canvas Skateboarding Shoe has a low-profile silhouette that delivers breathability, grip and responsive cushioning without sacrificing boardfeel. Streamlined canvas upper provides breathability. Zoom Air unit in the sockliner delivers responsive cushioning. Rubber outsole with herringbone tread pattern offers superior grip.'
+  description: 'Womens mid heel wedge shoes ladies diamante evening sparkly strappy sandals size. add a little bit of sparkle to your step with our wedge heel sandals. fasten around the ankle with an adjustable ankle strap and buckle closure. featuring high shine diamante detailing on the toe strap. perfect for parties, weddings, bridal, prom, evenings & more'
 },
 {
-  img: 'https://starpathdesign.files.wordpress.com/2014/10/leather-red-high-heel-shoes.jpeg',
-  name: 'Shining Stunner Red High Heels',
+  img: '../../../assets/leather-red-high-heel-shoes.jpg',
+  name: 'Shining Blown Stunner Red High Heels',
   type: 'High heels',
   price: '$ 62',
   size: 'Large',
   offer: '35%',
   category: 'Women',
   // tslint:disable-next-line: max-line-length
-  description: 'Special Technologies: Mens Nike SB Zoom Stefan Janoski Canvas Skateboarding Shoe LOW-PROFILE COMFORT. Mens Nike SB Zoom Stefan Janoski Canvas Skateboarding Shoe has a low-profile silhouette that delivers breathability, grip and responsive cushioning without sacrificing boardfeel. Streamlined canvas upper provides breathability. Zoom Air unit in the sockliner delivers responsive cushioning. Rubber outsole with herringbone tread pattern offers superior grip.'
+  description: 'HIGH HEELS FOR A FASHION HIGH It is a well-known fact that the best way to look attractive and to raise a woman’s glamour quotient is a pair of high heels. High heels are a quintessential part of every woman’s shoe closet.'
+},
+{
+  // tslint:disable-next-line: max-line-length
+  img: '../../../assets/Boot.jpg',
+  name: 'MERRELL Silversun Lace Waterproof Boots',
+  type: 'Boots',
+  price: '$ 90',
+  size: 'Medium',
+  offer: '20%',
+  category: 'Women',
+  // tslint:disable-next-line: max-line-length
+  description: 'Winter in the city wouldn’t be complete without a pair of boots that provide comfort and warmth, waterproof protection, and of course, style. The Silversun Lace Waterproof Boots doesn’t just check off your requirements - it redefines them.'
 }];

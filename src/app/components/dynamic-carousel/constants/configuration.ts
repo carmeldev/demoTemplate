e0c44b444c1cefa7318e8716b18c2ed777af9af3
carmeldevj <carmeldevj@gmail.com>
2020-03-05 16:02:55 +0530

    carousal constant added

---
+++ src/app/components/dynamic-carousel/constants/configuration.ts
@@ -0,0 +1,18 @@
+export class  CarousalConfiguration {
+
+    constructor() {}
+    carousalOptions = {
+        interval: '5000',
+        color : 'accent',
+        proportion : '45',
+        slidesCount : '5',
+        slides :[
+            {image: 'https://image.shutterstock.com/image-photo/bright-spring-view-cameo-island-260nw-1048185397.jpg'},
+            {image: 'https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569_960_720.jpg'},
+            {image: 'https://image.shutterstock.com/image-photo/bright-spring-view-cameo-island-260nw-1048185397.jpg'},
+            {image: 'https://image.shutterstock.com/image-photo/bright-spring-view-cameo-island-260nw-1048185397.jpg'},
+            {image: 'https://image.shutterstock.com/image-photo/bright-spring-view-cameo-island-260nw-1048185397.jpg'},
+          ]
+    }
+
+}
--- src/app/components/dynamic-carousel/dynamic-carousel.component.ts
+++ src/app/components/dynamic-carousel/dynamic-carousel.component.ts
@@ -1,20 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { MatCarousel, MatCarouselComponent } from '@ngmodule/material-carousel';
+import { CarousalConfiguration } from './constants/configuration';
 
 @Component({
   selector: 'app-dynamic-carousel',
   templateUrl: './dynamic-carousel.component.html',
   styleUrls: ['./dynamic-carousel.component.css']
 })
-export class DynamicCarouselComponent implements OnInit {
-  slides = [
-    {image: 'https://image.shutterstock.com/image-photo/bright-spring-view-cameo-island-260nw-1048185397.jpg'},
-    {image: 'https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569_960_720.jpg'},
-    {image: 'https://image.shutterstock.com/image-photo/bright-spring-view-cameo-island-260nw-1048185397.jpg'},
-    {image: 'https://image.shutterstock.com/image-photo/bright-spring-view-cameo-island-260nw-1048185397.jpg'},
-    {image: 'https://image.shutterstock.com/image-photo/bright-spring-view-cameo-island-260nw-1048185397.jpg'},
-  ]
-  constructor() { }
+export class DynamicCarouselComponent extends CarousalConfiguration implements OnInit {
+ 
+ 
+  @Input() interval : String;
+  @Input() color : String;
+  @Input() proportion : String;
+  @Input() slidesCount : String;
+  @Input() slides : any;
+
+ 
+ 
+  constructor() { 
+    super();
+    this.interval = this.interval? this.interval: this.carousalOptions.interval;
+    this.color = this.color? this.color: this.carousalOptions.color;
+    this.proportion = this.proportion? this.proportion: this.carousalOptions.proportion;
+    this.slidesCount = this.slidesCount? this.slidesCount: this.carousalOptions.slidesCount;
+    this.slides = this.slides? this.slides: this.carousalOptions.slides;
+}
 
   ngOnInit(): void {
   }
